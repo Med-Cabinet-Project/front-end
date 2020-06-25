772c8cf2a6e13b5cdde51c1aab611a422db773fb
gebhartn <nicholas.gebhart@gmail.com>
2020-06-24 20:05:49 -0700

    refactor: class -> functional

--- src/Components/RegisterForm.js
+++ src/Components/RegisterForm.js
@@ -1,86 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import { withRouter } from 'react-router-dom';
-import MedForm from './MedForm';
-import axiosWithAuth from '../utils/axiosWithAuth';
+import React, { useState, useEffect } from 'react'
+import { withRouter } from 'react-router-dom'
+// import MedForm from './MedForm';
+// import axiosWithAuth from '../utils/axiosWithAuth';
 
-function RegisterForm({history}) {
-    const inititalState= {
-        credentials: {
-            email:'',
-            password:''
-        },
-        medical: {},
-        effects:{}
-    };
+function RegisterForm({ history }) {
+  const initialState = {
+    email: '',
+    password: '',
+  }
 
-    const [register, setRegister] = useState(inititalState);
-    
-    useEffect(()=> {
-        localStorage.clear();
-    },[]); 
+  const [register, setRegister] = useState(initialState)
 
-    const handleChange = e => {
-        e.preventDefault();
-        const {name,value}=e.target
-        setRegister({...register, credentials:{[name]:value}})
-        console.log(register);
-    };
+  useEffect(() => {
+    localStorage.clear()
+  }, [])
 
-    const redirectToLogin = () => {
-        // props.updateTitle('Login')
-        history.push('/login');
-    };
-    
-    const handleSubmit = e => {
-        e.preventDefault();
+  const handleChange = e => {
+    e.preventDefault()
 
-        console.log(register)
-        // axiosWithAuth()
-        // .post('api/auth/register', register)
-        // .then(response => {
-        //     console.log(response.data);
-        //     localStorage.setItem('token', response.data.payload)
-        //     history.push('/protected')
-        // }) 
-        // .catch(error => {
-        //     console.log(error)
-        // })
-    };
+    setRegister({ ...register, [e.target.name]: e.target.value })
 
-        return(
-            <div className='register'>
-                <h1>Med-Cabinet</h1>
-            <div className='register-form'>
-                <h2>Register</h2>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor='email' />
-                        <input 
-                            type='email'
-                            id='email'
-                            placeholder='Email'
-                            value={register.credentials.email}
-                            onChange={handleChange}
-                            required
-                        />
-                        <input 
-                            type='password'
-                            id='password'
-                            placeholder='Password'
-                            value={register.credentials.password}
-                            onChange={handleChange}
-                            required
-                        />
-                        <button type='submit'>Register</button>
-                        {/* <MedForm register={register} /> */}
-                        
-                </form>
-                <div className='redirect'> 
-                    <p>Already have an account?</p>
-                    <span onClick={() => redirectToLogin()}>Login Here</span>
-                </div>
-            </div>
-            </div>
-        );
-};
+    console.log(register)
+  }
 
-export default withRouter(RegisterForm);
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    console.log({ register })
+  }
+
+  return (
+    <div className="register">
+      <h1>Med-Cabinet</h1>
+      <div className="register-form">
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email" />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={register.email}
+            onChange={e => handleChange(e)}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={register.password}
+            onChange={e => handleChange(e)}
+            required
+          />
+          <button type="submit">Register</button>
+        </form>
+        <div className="redirect">
+          <p>Already have an account?</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default withRouter(RegisterForm)
+
