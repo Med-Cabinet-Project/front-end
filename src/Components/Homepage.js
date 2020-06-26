5ed51435cdf1af44cdfa8b3b3d34548d3413e65a
gebhartn <nicholas.gebhart@gmail.com>
2020-06-25 20:52:48 -0700

    fix: refactor class -> function

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,39 +1,19 @@
-import React from 'react';
-import axiosWithAuth from '../utils/axiosWithAuth';
+import * as React from 'react'
+import axios from 'axios'
 
-class HomePage extends React.Component {
-    state = {strains: []}
+const HomePage = () => {
+  const [strains, setStrains] = React.useState([])
 
-    componentDidMount(){
-        this.getStrainsList();
-    }
-
-    getStrainsList = e => {
-        const token = JSON.parse(localStorage.getItem('token'))
-
-        axiosWithAuth()
-        .get('./api/strains')
-        .then(res => {
-            console.log(res);
-            this.setState({ strains: res.data })
-        })
-        .catch(error =>
-            console.log(error)
-            )
-    };
-
-    render(){
-        return(
-            <div className="home-profile">
-                <h1 className='h1-profile'>Welcome to Med-Cabinet!</h1>
-                <h2>User Profile</h2>
-                <h3>Recommended Strains</h3>
-                <p>*Strains from register form*</p>
-                <p>*Add/remove*</p>
-            </div>
-        )
-    }   
+  return (
+    <div className="home-profile">
+      <h1 className="h1-profile">Welcome to Med-Cabinet!</h1>
+      <h2>User Profile</h2>
+      <h3>Recommended Strains</h3>
+      <p>*Strains from register form*</p>
+      <p>*Add/remove*</p>
+    </div>
+  )
 }
 
+export default HomePage
 
-export default HomePage;
